d0316c074c8fa24f3e9c9dbce2104dc84da79b27
Oceanofprojects <115060426+Oceanofprojects@users.noreply.github.com>
2024-02-21 08:14:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hi, Team :
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          ~ We ready to launch tesseract ~
+          ~ We ready to launch tesseract
         </a>
       </header>
     </div>
