8c73f32401aacb016bce3f909e8fc8024a38229d
Oceanofprojects <115060426+Oceanofprojects@users.noreply.github.com>
2024-02-29 10:10:36 +0530

    Update index.js

--- src/Characters/index.js
+++ src/Characters/index.js
@@ -14,40 +14,40 @@ export default function Characters(){
 <h1 class="g-title">Cha<span style={{color:'darkred'}}>r</span>acters</h1>
 <br/>
     <section className="c-profile-layer">
-        <div className="c-profile char_1" style={{background:`url(game-assets/gg-design/characters/minister.jpg)`,backgroundPosition:'center',backgroundSize:'cover'}}>
+        <div className="c-profile char_1" style={{background:`url(../game-assets/gg-design/characters/minister.jpg)`,backgroundPosition:'center',backgroundSize:'cover'}}>
                 <span>
                     Minister
                 </span>
             <div className="corner-frame"></div>
         </div>
 
-        <div className="c-profile char_2" style={{background:`url(game-assets/gg-design/characters/king.jpg)`,backgroundPosition:'center',backgroundSize:'cover'}}>
+        <div className="c-profile char_2" style={{background:`url(../game-assets/gg-design/characters/king.jpg)`,backgroundPosition:'center',backgroundSize:'cover'}}>
                 <span>
                     King
                 </span>
             <div className="corner-frame"></div>
         </div>
 
-        <div className="c-profile char_2" style={{background:`url(game-assets/gg-design/characters/thief.jpg)`,backgroundPosition:'center',backgroundSize:'cover'}}>
+        <div className="c-profile char_2" style={{background:`url(../game-assets/gg-design/characters/thief.jpg)`,backgroundPosition:'center',backgroundSize:'cover'}}>
                 <span>
                     Thief
                 </span>
             <div className="corner-frame"></div>
         </div>
 
-        <div className="c-profile char_2" style={{background:`url(game-assets/gg-design/characters/queen.jpg)`,backgroundPosition:'center',backgroundSize:'cover'}}>
+        <div className="c-profile char_2" style={{background:`url(../game-assets/gg-design/characters/queen.jpg)`,backgroundPosition:'center',backgroundSize:'cover'}}>
                 <span>
                     Queen
                 </span>
             <div className="corner-frame"></div>
         </div>
-        <div className="c-profile char_2" style={{background:`url(game-assets/gg-design/characters/police.jpg)`,backgroundPosition:'center',backgroundSize:'cover'}}>
+        <div className="c-profile char_2" style={{background:`url(../game-assets/gg-design/characters/police.jpg)`,backgroundPosition:'center',backgroundSize:'cover'}}>
                 <span>
                     Police
                 </span>
             <div className="corner-frame"></div>
         </div>
-        <div className="c-profile char_2" style={{background:`url(game-assets/gg-design/characters/wizard.jpg)`,backgroundPosition:'center',backgroundSize:'cover'}}>
+        <div className="c-profile char_2" style={{background:`url(./game-assets/gg-design/characters/wizard.jpg)`,backgroundPosition:'center',backgroundSize:'cover'}}>
                 <span>
                     Wizard
                 </span>
