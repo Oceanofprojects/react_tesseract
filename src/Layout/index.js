81ea69a56b69be9ddf94e3198fd416710fa80139
Oceanofprojects <115060426+Oceanofprojects@users.noreply.github.com>
2024-03-01 12:14:44 +0530

    Update index.js

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,13 +1,7 @@
 export default function Back_Lay({attr}){
     return(
         <>
-        <h1>TESTTTTT :</h1>
-      <section style={{position:'fixed',
-top:'0',left:'0',
-backgroundImage:"url("+require(`${attr.url}`)+")",
-backgroundSize:'cover',backgroundPosition: 'top left',height:'100vh',width:'100%',zIndex:'-2'
-}}>
-      </section>
-          </>
+        <h1>TESTTTTT :{attr.url}</h1>
+           </>
     );
 }
