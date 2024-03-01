dfe4608e50da6d35248028f0428c57d2076d9a42
Oceanofprojects <115060426+Oceanofprojects@users.noreply.github.com>
2024-03-01 12:34:18 +0530

    Update index.js

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,10 +1,10 @@
 export default function Back_Lay({attr}){
     let data = "'"+attr.url+"'";
     return(
-        <>
-        <h1>ULRL :{data}</h1>
-           
-  
-           </>
+       <section style={{position:'fixed',
+top:'0',left:'0',
+backgroundImage:"url("+require(data)+")",
+backgroundSize:'cover',backgroundPosition: 'top left',height:'100vh',width:'100%',zIndex:'-2'
+}}></section>
     );
 }
