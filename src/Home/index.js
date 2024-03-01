93965d375e9c06dfa8f934facb2c420bc8bf0d70
Oceanofprojects <115060426+Oceanofprojects@users.noreply.github.com>
2024-03-01 12:08:08 +0530

    Update index.js

--- src/Home/index.js
+++ src/Home/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="center-content">
     <h1 className="g-title">Raja&nbsp;<b style={{color:'red'}}>&hearts;</b>&nbsp;Rani</h1>
     <DryLeafLayer/>
-    <Back_Lay attr={{url:'../game-assets/gg-design/bg1.jpg'}} />
+    <Back_Lay attr={{url:'game-assets/gg-design/bg1.jpg'}} />
         <h6 style={{textAlign:'center',padding:'20px 0px',color:'#fff'}}>New multiplayer. V.0.11</h6>
         <center>
         <button className="active-btn" onClick={()=>window.open('/CreateRoom','_self')}>Create room</button>
