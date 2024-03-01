75a408bb91b7b537a3e098e371464039f350756b
Oceanofprojects <115060426+Oceanofprojects@users.noreply.github.com>
2024-03-01 11:52:51 +0530

    Update index.js

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -2,7 +2,8 @@ export default function Back_Lay({attr}){
     return(
       <section style={{position:'fixed',
 top:'0',left:'0',
-background:"linear-gradient(rgba(0, 0,0,.7), rgba(0,0,0,.4), rgba(0,0,0,.7)), url("+{require(`${attr.url}`)}+")",backgroundSize:'cover',backgroundPosition: 'top left',height:100vh',width:'100%',zIndex:'-2'
+backgroundImage:"linear-gradient(rgba(0, 0,0,.7), rgba(0,0,0,.4), rgba(0,0,0,.7)), url("+{require(`${attr.url}`)}+")",
+backgroundSize:'cover',backgroundPosition: 'top left',height:100vh',width:'100%',zIndex:'-2'
 }}>
       </section>
     );
